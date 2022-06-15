df7ab02ca4de0e7df5c123be3d210129b5809ea1
naxrohan <rohan.jd@hotmail.com>
2022-06-15 17:08:59 +0100

    middleware working

--- src/AuthToken.php
+++ src/AuthToken.php
@@ -4,6 +4,9 @@ namespace Drupal\user_link;
 use Drupal;
 use Drupal\user\Entity\User;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 /**
  * AuthToken class to handle tokens 
  */
@@ -11,15 +14,28 @@ class AuthToken {
 
     private $token = null;
     private $token_length;
-
+    
+    /**
+     * define the field name
+     * @var type
+     */
+    private $field_name = "field_auth_token";
+    
     function __construct(int $length = 32) {
         $this->token = null;
         $this->token_length = $length;
     }
 
     public function createToken() {
-        $length = $this->token_length;
-        $this->token = rand(intval(pow(10, $length-1)), intval(pow(10, $length))-1);
+        mt_srand(rand(111111, 999999));
+        $count = 0;
+        $full_number = '';
+        while($count < $this->token_length) {
+            $full_number .= sprintf('%02d',mt_rand(01, 99));
+            $count++;
+        }
+
+        $this->token = $full_number;
     }
 
     public function getNewToken() {
@@ -30,21 +46,50 @@ class AuthToken {
 
     public function assignToken() {
         $ids = Drupal::entityQuery('user')
-            ->condition('status', 1)
-//            ->condition('roles', 'administrator', '!=')
+            ->condition('status', 1) // include all active users only
+        //    ->condition('roles', 'administrator','=') //exclude/include accounts by role..
             ->execute();
-        
+
         $users = User::loadMultiple($ids);
         print("\n Total Users::". count($users));
-        
+
         foreach ($users as $user){
-            //todo: check if the topken already exists
-            
-            // updated the auth token fields for the user
-            $randToken = $this->getNewToken();
-            $user->set('field_auth_token2', $randToken);
-            print("\n Token Updated..");
+            $uid = $user->id();
+
+            if (!in_array($uid, array(0, 1))) {
+                //todo: check if the token already exists
+                // var_dump($account->getAuthToken());
+                $account = User::load($uid);
+
+                // updated the auth token fields for the user
+                $randToken = $this->getNewToken();
+                $account->set($this->field_name, $randToken);
+                print("\n Token Updated..");
+                $account->save();
+            }
         }
+
+    }
+
+    /**
+     * Lookup toke in db
+     * @param type $_token
+     * @return type
+     */
+    public function lookUpToken($_token){
         
+        $ids = Drupal::entityQuery('user')
+            ->condition('status', 1) // include all active users only
+            ->condition('field_auth_token', $_token)
+            ->range(0, 1)
+            ->execute();
+    
+        if(count($ids) > 0){
+            return array_pop($ids);
+        }
+        return null;
     }
 }
+
+// $tokenHandler = new AuthToken();
+// $tokenHandler->assignToken();
--- src/AuthTokenLogin.php
+++ src/AuthTokenLogin.php
@@ -1,6 +1,8 @@
 <?php
 namespace Drupal\user_link;
 
+use Drupal\user\Entity\User;
+use Drupal\user_link\AuthToken;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
@@ -10,7 +12,13 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
  * @author Rohan
  */
 class AuthTokenLogin implements HttpKernelInterface {
-    
+
+    /**
+     *
+     * @var type
+     */
+    private $argument_name = 'authtoken';
+
     /**
      * The kernel.
      *
@@ -27,25 +35,34 @@ class AuthTokenLogin implements HttpKernelInterface {
     public function __construct(HttpKernelInterface $http_kernel) {
         $this->httpKernel = $http_kernel;
     }
-    
+
+    /**
+     *
+     * @param Request $request
+     * @param type $type
+     * @param type $catch
+     * @return \Drupal\user_link\Response
+     */
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true) {
         $urlStr = $request->getUri();
-        
         $url_parts = parse_url($urlStr);
         parse_str($url_parts['query'], $query);
-        
-        
-        if(isset($query['authtoken'])){
-            $auth_token = $query['authtoken'];
-            
+        if(isset($query[$this->argument_name])){
+            $auth_token = $query[$this->argument_name];
+
             //todo: lookup & validate
-            //todo: do login for the user account & redirect to the home page..?
-            var_dump($auth_token);
-            var_dump($request->getClientIp());
-            
-            return new Response($this->t('hello world!! authtoken found...'), 403);
-            
+            $auth = new AuthToken();
+            $userFound = $auth->lookUpToken($auth_token);
+            if(isset($userFound)){
+                //fixme: do login for the user account & redirect to the home page..?
+                $account = User::load($userFound);
+//                user_login_finalize($account);
+//                dump($account);
+//                return RedirectResponse::create(Url::fromUri('<front>'),302);
+            }
+
         }
+         return $this->httpKernel->handle($request, $type, $catch);
     }
 
 }
